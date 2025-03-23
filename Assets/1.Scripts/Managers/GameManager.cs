a41c76c47f627089a26571489fcf8d472e7f6127
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-03-23 13:19:18 +0900

    chore

--- Assets/1.Scripts/Managers/GameManager.cs
+++ Assets/1.Scripts/Managers/GameManager.cs
@@ -1,4 +1,6 @@
+using System.Collections;
 using System.Threading.Tasks;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
@@ -6,13 +8,14 @@ public class GameManager : Singleton<GameManager>
     public bool isInit = false; //앱 초기화
     public static bool isGameStart = false; //게임 시작
 
-
+    private Coroutine LoadMapCoroutine;
+    private const float loadMapElapse = 15f;
 
     #region Unity Life Cycles
     protected override void Awake()
     {
         base.Awake();
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 60;
         Application.runInBackground = true;
     }
 
@@ -32,25 +35,35 @@ public class GameManager : Singleton<GameManager>
 #endif
 
         await InitManagers();
-        Time.timeScale = 0;
+        //Time.timeScale = 0;
 
         isInit = true;
     }
 
     public void StartGame()
     {
-        //커서 On
+        //Fade in & out
+
         //풍선 위치 : -14
         //카메라 위치 : -12
         //TimeScale = 1
-        
-        //카메라&풍선 이동 연출
+
+        //커서 On
+
+        //카메라&풍선 이동 연출 (풍선 먼저 떠오르기 시작)
         //적정 위치에 stop
+        //풍선 위치 : -2.5
+        //카메라 : 0
+
         //맵 소환술
+        LoadMapCoroutine = StartCoroutine(LoadMap());
     }
 
     public void GameOver()
     {
+        //맵 생성 정지
+        StopCoroutine(LoadMapCoroutine);
+
         //커서 Off
         //풍선 터지는 모션
         //TimeScale = 0
@@ -63,6 +76,15 @@ public class GameManager : Singleton<GameManager>
         await ResourceManager.Instance.Init();
         await UIManager.Instance.Init();
         await MapManager.Instance.Init();
+    }
+
+    private IEnumerator LoadMap()
+    {
+        WaitForSeconds mapElapse = new(loadMapElapse);
+        while (true)
+        {
+            yield return mapElapse;
+        }
     }
     #endregion
 }
