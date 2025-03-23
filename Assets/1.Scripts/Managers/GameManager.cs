c8ae10ffbd99295ff116f520f5f84f47f35e3a24
Charen523 <144107013+Charen523@users.noreply.github.com>
2025-03-23 11:16:44 +0900

    update : 기본 함수 추가, isInit = true 추가

--- Assets/1.Scripts/Managers/GameManager.cs
+++ Assets/1.Scripts/Managers/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : Singleton<GameManager>
     public bool isInit = false; //앱 초기화
     public static bool isGameStart = false; //게임 시작
 
+
+
+    #region Unity Life Cycles
     protected override void Awake()
     {
         base.Awake();
@@ -17,7 +20,9 @@ public class GameManager : Singleton<GameManager>
     {
         isInit = false;
     }
+    #endregion
 
+    #region Main Methods
     public async Task InitApp()
     {
 #if UNITY_EDITOR
@@ -27,14 +32,37 @@ public class GameManager : Singleton<GameManager>
 #endif
 
         await InitManagers();
+        Time.timeScale = 0;
+
+        isInit = true;
     }
 
+    public void StartGame()
+    {
+        //커서 On
+        //풍선 위치 : -14
+        //카메라 위치 : -12
+        //TimeScale = 1
+        
+        //카메라&풍선 이동 연출
+        //적정 위치에 stop
+        //맵 소환술
+    }
+
+    public void GameOver()
+    {
+        //커서 Off
+        //풍선 터지는 모션
+        //TimeScale = 0
+    }
+    #endregion
+
+    #region Sub Methods
     private async Task InitManagers()
     {
-        // Initialize ResourceManager
         await ResourceManager.Instance.Init();
-
-        // Initialize UIManager
         await UIManager.Instance.Init();
+        await MapManager.Instance.Init();
     }
+    #endregion
 }
